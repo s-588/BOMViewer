49d2e9f9ae5848029754b0169e83ba5ba94e04eb
s-588 <sergeivanov827@gmail.com>
2025-10-27 11:59:10 +0300

    update schema

--- internal/db/schema.sql
+++ internal/db/schema.sql
@@ -1,27 +1,36 @@
 CREATE TABLE materials (
-    material_id SERIAL PRIMARY KEY,
-    unit VARCHAR(50) NOT NULL,                   
+    material_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    unit INT NOT NULL REFERENCES unit_types(unit_id) ON DELETE SET NULL,                   
     description TEXT                           
 );
 
+
 CREATE TABLE material_names (
-    name_id SERIAL PRIMARY KEY,
+    name_id INTEGER PRIMARY KEY AUTOINCREMENT,
     material_id INT NOT NULL REFERENCES materials(material_id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     is_primary BOOLEAN NOT NULL DEFAULT FALSE,
     UNIQUE(material_id, name)
 );
+-- allow only one true value of is_primary per group of same material_id's
+CREATE UNIQUE INDEX unique_primary_name_per_material
+ON material_names(material_id) WHERE is_primary = 1;
 
 CREATE TABLE products (
-    product_id SERIAL PRIMARY KEY,
+    product_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255) NOT NULL UNIQUE,
-    description TEXT,
-    price NUMERIC(12,2) NOT NULL CHECK (price >= 0)
+    description TEXT
 );
 
 CREATE TABLE product_materials (
     product_id INT REFERENCES products(product_id) ON DELETE CASCADE,
     material_id INT REFERENCES materials(material_id) ON DELETE RESTRICT,
-    quantity NUMERIC(12,3) NOT NULL CHECK (quantity > 0),
+    quantity NUMERIC(12,3) CHECK (quantity > 0),
+    quantity_text VARCHAR(50),
     PRIMARY KEY (product_id, material_id)
+);
+
+CREATE TABLE unit_types(
+    unit_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    unit VARCHAR(50) NOT NULL
 );
